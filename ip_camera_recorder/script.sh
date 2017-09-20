37cfa0582f1edb8881be61c16ff024da5e9400c2
toru <thorgeo65@gmail.com>
2017-09-20 09:50:01 +0900

    Fix script.sh

--- ip_camera_recorder/script.sh
+++ ip_camera_recorder/script.sh
@@ -2,4 +2,4 @@ ffmpeg \
   -i $RTSP_URL \
   -vcodec copy -strict -2 \
   -f segment -segment_time 60 -segment_format mp4 -segment_list_flags +live \
-  -segment_list /records/output.m3u8 /records/out%08d.ts
+  -segment_list /records/output.m3u8 /records/`date +"%Y%m%d_%H%M"`_%08d.ts
